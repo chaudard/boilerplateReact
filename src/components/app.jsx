8209d6da16830d0ba693c41fde4a5722bb30dc95
MICHEL Dany <donnetonfalzard+gitlab@qmail.com>
2024-12-30 18:38:50 +0100

    (forgotten) first react component (add)

---
+++ src/components/app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+    return(
+        <div className="app">
+            <div className="btn-container">
+                <button className="btn btn-primary">Click me.</button>
+            </div>
+        </div>
+    )
+}
+
+export default App;
