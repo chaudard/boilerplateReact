9fb82a60e4052e0082a0383bbfcd699194a37a9e
MICHEL Dany <donnetonfalzard+gitlab@qmail.com>
2024-12-30 20:20:04 +0100

    add simple title

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const App = () => {
     return(
         <div className="app">
+            <h1 className="text-center">First react app</h1>
             <div className="btn-container">
                 <button className="btn btn-primary">Click me.</button>
             </div>
